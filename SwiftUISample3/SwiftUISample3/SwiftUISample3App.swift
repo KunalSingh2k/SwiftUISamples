34baba483377793fe0f4905ba0c58495ad998053
KunalSingh2k <145484775+KunalSingh2k@users.noreply.github.com>
2024-08-26 11:43:26 +0530

    Add files via upload

---
+++ SwiftUISample3/SwiftUISample3/SwiftUISample3App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISample3App.swift
+//  SwiftUISample3
+//
+//  Created by Kunal Kumar R on 20/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISample3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

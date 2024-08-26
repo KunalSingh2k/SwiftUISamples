345ed0ef7ce3c522216a0b7a6ef88f57d91f29b6
KunalSingh2k <145484775+KunalSingh2k@users.noreply.github.com>
2024-08-26 11:42:56 +0530

    Add files via upload

---
+++ SwiftUISample2/SwiftUISample2/ContentView.swift
@@ -0,0 +1,106 @@
+//
+//  ContentView.swift
+//  SwiftUISample2
+//
+//  Created by Kunal Kumar R on 20/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Monaco", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+    
+    
+    var body: some View {
+        ZStack {
+//            LinearGradient(colors: [.blue, .black], startPoint: .topLeading, endPoint: .bottomTrailing)
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 300)
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Text("Guess The Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.white)
+                
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap on the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.bold))
+                    }
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        }label: {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 5)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(score)")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                
+                Spacer()
+            }
+            .padding()
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            if score == 8 {
+                Button("Restart") { askNewQuestion() }
+            }else {
+                Button("Continue") { askNewQuestion() }
+            }
+            
+        }message: {
+            if score == 8 {
+                Text("Finished! Do you wanna play again?")
+            }else {
+                Text("Your score is \(score)")
+            }
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Yay! That's the flag of \(countries[correctAnswer])"
+            score += 1
+        }else {
+            scoreTitle = "Wrong! That's the flag of \(countries[correctAnswer])"
+            if score == 0 {
+                score = 0
+            }else {
+                score = score
+            }
+        }
+        showingScore = true
+    }
+    
+    func askNewQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUISample2/SwiftUISample2/SwiftUISample2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISample2App.swift
+//  SwiftUISample2
+//
+//  Created by Kunal Kumar R on 20/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISample2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
